1a8c1c3b86085a49b5b7922afdce309d69037519
DUY BACH DANG <104045937@student.swin.edu.au>
2024-06-06 19:27:46 +0700

    change guess input to type of text, and validate input

--- week4/41P/framework/js/appgame.js
+++ week4/41P/framework/js/appgame.js
@@ -8,12 +8,18 @@ Vue.createApp({
     },
     methods: {
         checkNumber() {
-            if (this.guess == this.result) {
-                this.message = "You got it!";
-            } else if (this.guess < this.result) {
-                this.message = "Guess higher";
+            if (isNaN(this.guess)) {
+                this.message = "Please enter a valid number";
+            } else if (parseInt(this.guess) < 1 || parseInt(this.guess) > 100) {
+                this.message = "Please enter a number between 1 and 100";
             } else {
-                this.message = "Guess lower";
+                if (this.guess == this.result) {
+                    this.message = "You got it!";
+                } else if (this.guess < this.result) {
+                    this.message = "Guess higher";
+                } else {
+                    this.message = "Guess lower";
+                }
             }
         },
         giveUp() {
